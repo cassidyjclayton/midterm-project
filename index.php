b53c5e1afb1a3003b433fea65e49e252e0e4a213
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-10-30 02:15:02 -0400

    made thing php,
    please oh please don't screw me up

---
+++ index.php
@@ -0,0 +1,42 @@
+<? $PageTitle = "girls-on-the-rocks"; ?>
+<?php include "inc/html-top"  ?>
+<body class="z-patttern">
+	
+	
+		<header class="persistent">
+			<div class="container">
+				<h1 class="lead">Girls on the Rocks</h1>
+			<section class="strong-follow">
+				Your site for keeping up with female climbers.
+				<p>
+					<a href="https://www.instagram.com/thewildheartedwanderess/" class="fa fa-instagram"> 
+					</a>
+					Follow the site creator @thewildheartedwanderess 
+				</p>
+			</section>
+			</div>
+		</header>
+		
+		<main>
+			<div id="big-message" class="hero"> 
+				<h2>Featuring top US climbers!</h2>
+			</div>
+		</main>
+
+		<footer class="persistent">
+			<div class="container">
+				<section class="weak-follow">
+				Keep updated with our newsletter!
+					<form> 
+						Email <input type="text" name="email">
+						<input type="submit" name="submit">
+					</form>
+				</section>
+				<h2 class="terminal">
+					<a href="profiles.html" class="non-icon-link">Climber Profiles →</a> 
+				</h2>
+			</div>
+		</footer>
+	
+</body>
+</html>
